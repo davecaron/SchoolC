5143ebee99e0621a0784444dd0722c69a82c1cdc
Fausateeeee <pogo527@hotmail.com>
2017-04-16 22:35:55 -0400

    Update PersonneException.h

--- PersonneException.h
+++ PersonneException.h
@@ -1,1 +1,24 @@
 
+/**
+ * \file PersonneException.h
+ * \brief Header de la classe Personne
+ * \author Charles-David Lachance
+ */
+
+
+#ifndef PERSONNEEXCEPTION_H_
+#define PERSONNEEXCEPTION_H_
+
+namespace tp{
+  
+  class PersonneException public std::runtime_error
+  {
+  public:
+  PersonneException(const std::string& raison);
+  
+  private:
+  std::string& raison;
+  };
+  }
+
+#endif
