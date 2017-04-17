92ad3c059294259c554e1e24193ee8b5fbb7b8f0
Fausateeeee <pogo527@hotmail.com>
2017-04-16 23:44:07 -0400

    Update PersonneDejaPresenteException.h

--- PersonneDejaPresenteException.h
+++ PersonneDejaPresenteException.h
@@ -1,1 +1,23 @@
+/**
+ * \file PersonneException.h
+ * \brief Header de la classe Personne
+ * \author Charles-David Lachance et Dave Caron
+ */
 
+
+#ifndef PERSONNEDEJAPRESENTEEXCEPTION_H_
+#define PERSONNEDEJAPRESENTEEXCEPTION_H_
+
+namespace tp{
+  
+  class PersonneException public PersonneException
+  {
+  public:
+  PersonneException(const std::string& raison);
+  
+  private:
+  std::string& raison;
+  };
+  }
+
+#endif
