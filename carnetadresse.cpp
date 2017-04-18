86726ea410892d73f81127cb2d520204b437d5c8
Fausateeeee <pogo527@hotmail.com>
2017-04-18 15:26:17 -0400

    Update carnetadresse.cpp

--- carnetadresse.cpp
+++ carnetadresse.cpp
@@ -1,34 +1,31 @@
 #include "carnetadresse.h"
 #include "annuaire.h"
 #include <QtGui>
+#include <QDialog>
 
 CarnetAdresse::CarnetAdresse(QWidget *parent)
     : QWidget(parent)
 {
-    QLabel *nameLabel = new QLabel(tr("Nom:"));
-    nameLine = new QLineEdit;
-    nameLine->setReadOnly(true);
 
-    QLabel *addressLabel = new QLabel(tr("Annuaire:"));
+    QLabel *addressLabel = new QLabel(tr("Personne:"));
     addressText = new QTextEdit;
     addressText->setReadOnly(true);
         
-    addButton = new QPushButton(tr("&Ajouter"));
-    addButton->show();
-    submitButton = new QPushButton(tr("&Soumettre"));
-    submitButton->hide();
-    cancelButton = new QPushButton(tr("&Annuler"));
-    cancelButton->hide();
+    ajouterButton = new QPushButton(tr("&Ajouter"));
+    ajouterButton->show();
+    suivantButton = new QPushButton(tr("&Suivant"));
+    suivantButton->show();
+    precedentButton = new QPushButton(tr("&Précédent"));
+    precendentButton->show();
         
     QVBoxLayout *buttonLayout1 = new QVBoxLayout;
-    buttonLayout1->addWidget(addButton, Qt::AlignTop);
-    buttonLayout1->addWidget(submitButton);
-    buttonLayout1->addWidget(cancelButton);
+    buttonLayout1->addWidget(ajouterButton, Qt::AlignTop);
+    buttonLayout1->addWidget(suivantButton);
+    buttonLayout1->addWidget(precedentButton);
     buttonLayout1->addStretch();
     
     QGridLayout *mainLayout = new QGridLayout;
-    mainLayout->addWidget(nameLabel, 0, 0);
-    mainLayout->addWidget(nameLine, 0, 1);
+
     mainLayout->addWidget(addressLabel, 1, 0, Qt::AlignTop);
     mainLayout->addWidget(addressText, 1, 1);
     mainLayout->addLayout(buttonLayout1, 1, 2);
