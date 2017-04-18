849f440dbc52f966115db54ea1cc959f3ef99222
Fausateeeee <pogo527@hotmail.com>
2017-04-18 03:23:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,16 @@
 #include <QApplication>
+#include <QFont> 
 #include <QPushButton>
+#include <QWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+  
+    QPushButton quit("Quitter");
+    quit.resize(75, 30);
+    quit.setFont(QFont("Times", 18, QFont::Bold));
+    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
+
+    quit.show();
+    return app.exec();
