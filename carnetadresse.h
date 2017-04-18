9d10d18883bc585d2976c9a003078b7343bd1826
Fausateeeee <pogo527@hotmail.com>
2017-04-18 14:43:59 -0400

    Update carnetadresse.h

--- carnetadresse.h
+++ carnetadresse.h
@@ -1,12 +1,20 @@
-
 class CarnetAdresse : public QWidget
 {
     Q_OBJECT
 
 public:
     CarnetAdresse(QWidget *parent = 0);
+    void addContact();
+    void submitContact();
+    void cancel();
 
 private:
+    QPushButton *addButton;
+    QPushButton *submitButton;
+    QPushButton *cancelButton;
     QLineEdit *nameLine;
     QTextEdit *addressText;
+    QMap<QString, QString> contacts;
+    QString oldName;
+    QString oldAddress;
 };
